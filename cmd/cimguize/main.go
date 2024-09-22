58599b89430caf2791a2e638da778620153a127e
gucio321 <73652197+gucio321@users.noreply.github.com>
2024-09-22 21:10:12 +0200

    cmd: write options

--- cmd/cimguize/main.go
+++ cmd/cimguize/main.go
@@ -11,6 +11,8 @@ import (
 
 func main() {
 	path := flag.String("path", "", "Path the .h file")
+	funcsOutputPath := flag.String("funcs", "", "Path to the output file with functions (definitions.json)")
+	samOutputPath := flag.String("sam", "", "Path to Structs And Enums json output (structs_and_enums.json)")
 	flag.Parse()
 
 	if len(*path) == 0 {
@@ -26,6 +28,36 @@ func main() {
 	c, err := cimguizer.Parse(fileData)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	// write funcs
+	if len(*funcsOutputPath) > 0 {
+		funcs, err := c.Functions()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = os.WriteFile(*funcsOutputPath, funcs, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Functions saved to", *funcsOutputPath)
+	}
+
+	// write structs and enums
+	if len(*samOutputPath) > 0 {
+		sam, err := c.StructAndEnums()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = os.WriteFile(*samOutputPath, sam, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Structs and Enums saved to", *samOutputPath)
 	}
 
 	sam, _ := (c.StructAndEnums())
